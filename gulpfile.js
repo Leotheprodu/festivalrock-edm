e67cbd8327b1ecfe22e6e42f62b2b8ab9fa42b44
Leo Serrano <leovpc@gmail.com>
2022-09-25 00:29:36 -0600

    seguimos trabajando

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+//CSS
+
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+//Imagenes
+const cache =require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+
 
 function css(done) {
     src('src/scss/**/*.scss') //ideantificar archivo de sass
@@ -10,6 +18,28 @@ function css(done) {
 
     done();
 }
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{jpg,png}')
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest('build/img'))
+
+    done()
+}
+
+function versionWebp(done) {
+    const opciones = {
+        quality: 50
+    };
+    
+    src('src/img/**/*.{jpg,png}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+
+    done()
+}
 
 function dev(done){
     watch('src/scss/**/*.scss',css)
@@ -18,5 +48,8 @@ function dev(done){
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, dev);
+
 
