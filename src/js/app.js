f63a607e1ee6b46574fbdeb679ed79b7eb77e633
Leo Serrano <leovpc@gmail.com>
2022-09-29 17:26:34 -0600

    listo el header flotante al dar scroll

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,33 @@ document.addEventListener('DOMContentLoaded', function(){
     iniciarApp();
 });
 function iniciarApp() {
+    navegacionFija();
     crearGaleria();
+    scrollNav();
+}
+
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+    window.addEventListener('scroll', function(){
+
+        if (sobreFestival.getBoundingClientRect().bottom < 0 ){
+            barra.classList.add('fijo')
+        } else {
+            barra.classList.remove('fijo')
+        }
+    });
+}
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', function(e) {
+            e.preventDefault();
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({behavior: "smooth"});
+        });
+    });
 }
 
 function crearGaleria() {
