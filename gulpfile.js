7b6bbc6ef8c52e7365f935d5a9eb659c1ba49ea3
Leo Serrano <leovpc@gmail.com>
2022-09-30 11:55:20 -0600

    terminado el proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const { src, dest, watch, parallel } = require('gulp');
-//CSS
 
+//CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 //Imagenes
 const cache =require('gulp-cache');
@@ -10,17 +14,26 @@ const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif =require('gulp-avif');
 
+//JavaScript
+const terser = require('gulp-terser-js');
+
 
 function css(done) {
     src('src/scss/**/*.scss') //ideantificar archivo de sass
-        .pipe(plumber()) 
+        .pipe(sourcemaps.init()) //crear el source map
+        .pipe(plumber()) //para que siga ejecutando aunque hayan errores
         .pipe(sass()) //compilarlo
+        .pipe(postcss([ autoprefixer(), cssnano() ])) //mejoras de rendimiento del css
+        .pipe(sourcemaps.write('.')) //escribe el source map
         .pipe(dest('build/css')) //almacenar en hd
 
     done();
 }
 function javascript(done) {
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'))
     done()
 }
